d368630eb833d7c9b5f9fa59099eff7479c1d048
Habeebllah <lasisihabeeb67@gmail.com>
2022-11-17 15:36:55 +0100

    17/11/2022

---
+++ app/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.utils.text import slugify
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(blank=True, null=True, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = ("Category")
+        verbose_name_plural = ("Categorys")
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        value = self.name
+        self.slug = slugify(value, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+class Post(models.Model):
+    STATUS = (
+        ('Publish', 'Publish'),
+        ('Draft', 'Draft')
+    )
+    title = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='pic/')
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    approval = models.BooleanField(default=False)
+    status = models.CharField(max_length=50, choices=STATUS)
+    slug = models.SlugField(blank=True, null=True, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = ("Post")
+        verbose_name_plural = ("Posts")
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        value = self.title
+        self.slug = slugify(value, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    comment = models.TextField()
+
+    class Meta:
+        verbose_name = ("Comment")
+        verbose_name_plural = ("Comments")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
